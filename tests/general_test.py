6f6bdcf6b7bc901a71d6575dd6eec298d8d4e537
Miroslav Jiřík <miroslav.jirik@gmail.com>
2021-04-07 16:00:44 +0200

    api

--- tests/general_test.py
+++ tests/general_test.py
@@ -1,5 +1,27 @@
 import pytest
 import vesselregistration.moduleone
+import vesselregistration.registration
+import numpy as np
 
 def test_hello():
     vesselregistration.moduleone.print_hello("Vlkoslav")
+
+
+def test_vr():
+    volume1 = np.zeros([50, 51, 52])
+    volume1[30:40, 30:40, 10:40] = 1
+    volume1[30:38, 10:38, 30:38] = 1
+
+    volume2 = np.zeros([500, 510, 520])
+    volume2[300:400, 300:400, 100:400] = 1
+    volume2[300:380, 100:380, 300:380] = 1
+
+    registrator = vesselregistration.registration.VesselRegistraion()
+    tt1 = registrator.create_tube_table(volume1)
+    tt2 = registrator.create_tube_table(volume2)
+
+    kp1 = registrator.create_keypoint_table(tt1)
+    kp2 = registrator.create_keypoint_table(tt2)
+
+    tfmatrix = registrator.registration(kp1, kp2)
+    pass
---
+++ vesselregistration/registration.py
@@ -0,0 +1,34 @@
+from typing import Union
+import numpy as np
+
+
+
+class VesselRegistraion(object):
+
+    def __init__(self):
+        pass
+
+    def registration(self, path_to_tube_table1: str, path_to_tube_table2: str) -> np.ndarray:
+        """
+
+        :param path_to_tube_table1:
+        :param path_to_tube_table2:
+        :return: transform matrix
+        """
+
+
+        pass
+
+    def create_tube_table_from_file(self, path_to_image1, threshold):
+        pass
+
+    def create_tube_table(self, binary_image):
+        tube_table = None
+
+        return tube_table
+
+    def create_keypoint_table(self, path_to_tube_table):
+        pass
+
+    def transform(self):
+        pass
